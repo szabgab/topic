67b3858e8b0ac23f8c34d15b222872f2503147d7
Gabor Szabo <gabor@szabgab.com>
2015-01-26 16:10:06 +0200

    add empty main module; add initial test script loading the Crawler

---
+++ lib/Topic.pm
@@ -0,0 +1,9 @@
+package Topic;
+use Moose;
+use 5.010;
+
+# ABSTRACT: An experimental Topic
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
---
+++ t/00-topic.t
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+plan tests => 1;
+
+use Topic::Crawler;
+
+pass;
